ac4dd7d716ef0ed0dfdcbf246df416dfe39e01da
dr-js <dr@dr.run>
2022-06-05 15:10:30 +0000

    CHG: ci script update

--- bash/verify-alias.js
+++ bash/verify-alias.js
@@ -1,26 +1,25 @@
-const { promises: fsAsync } = require('fs')
-
-const { forEachRegExpExec } = require('@dr-js/core/library/common/string')
-const { getFileList } = require('@dr-js/core/library/node/file/Directory')
-const { runMain } = require('@dr-js/dev/library/main')
+const { forEachRegExpExec } = require('@dr-js/core/library/common/string.js')
+const { readTextSync } = require('@dr-js/core/library/node/fs/File.js')
+const { getFileListSync } = require('@dr-js/core/library/node/fs/Directory.js')
+const { runKit } = require('@dr-js/core/library/node/kit.js')
 
 // parse and check to prevent alias overwrite
 const PATH_ROOT = __dirname
 const REGEXP_ALIAS_FUNCTION = /(?:alias ([\w-]+)=|function ([\w-]+) \{)/g
 
-runMain(async (logger) => {
-  const bashScriptFileList = (await getFileList(PATH_ROOT)).filter((name) => name.endsWith('.sh'))
-  logger.stepLog(`found ${bashScriptFileList.length} bash script`)
+runKit(async (kit) => {
+  const bashScriptFileList = getFileListSync(PATH_ROOT).filter((name) => name.endsWith('.sh'))
+  kit.stepLog(`found ${bashScriptFileList.length} bash script`)
 
   const aliasMap = new Map()
   for (const bashScriptFile of bashScriptFileList) {
-    logger.log('[bashScriptFile]', bashScriptFile)
-    forEachRegExpExec(REGEXP_ALIAS_FUNCTION, String(await fsAsync.readFile(bashScriptFile)), ([ , name0, name1 ]) => {
+    kit.log('[bashScriptFile]', bashScriptFile)
+    forEachRegExpExec(REGEXP_ALIAS_FUNCTION, readTextSync(bashScriptFile), ([ , name0, name1 ]) => {
       const aliasName = name0 || name1
-      logger.log('  [aliasName]', aliasName)
+      kit.log('  [aliasName]', aliasName)
       if (aliasMap.has(aliasName)) throw new Error(`duplicate aliasName "${aliasName}" from: "${aliasMap.get(aliasName)}" and "${bashScriptFile}"`)
       else aliasMap.set(aliasName, bashScriptFile)
     })
   }
-  logger.stepLog(`verified ${aliasMap.size} alias`)
-}, 'verify-alias')
+  kit.stepLog(`verified ${aliasMap.size} alias`)
+}, { title: 'verify-alias' })
